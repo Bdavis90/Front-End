baa6cb48ede5e360e19492f89ab98d6e92eb3d07
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2019-10-22 00:07:52 -0400

    added request to add and delete topNine

--- my-top-nine/src/components/Navigation.js
+++ my-top-nine/src/components/Navigation.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 // import Form from 'react-bootstrap/Form';
 // import Button from 'react-bootstrap/Button';
 
 const Navigation = () => {
-    return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand inline href="#home">My Top Nine</Navbar.Brand>
-            <Nav className="mr-auto" inline>
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#signup">Sign Up</Nav.Link>
-                <Nav.Link href="#login">Log In</Nav.Link>
-            </Nav>
-        {/* <Form inline>
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand inline href="#home">
+        My Top Nine
+      </Navbar.Brand>
+      <Nav className="mr-auto" inline>
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#signup">Sign Up</Nav.Link>
+        <Nav.Link href="#login">Log In</Nav.Link>
+      </Nav>
+      {/* <Form inline>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
         <Button variant="outline-success">Search</Button>
         </Form> */}
-        </Navbar>
-
-
-    );
-}
+    </Navbar>
+  );
+};
 
 export default Navigation;
